298441630227f5e2e853852a74aa0042c3d8537c
Arnaud MOREL <a.morel@hotmail.com>
2014-04-01 12:30:10 +0200

    Init

---
+++ HomeRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeRental.Startup))]
+namespace HomeRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
