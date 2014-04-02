227a51d287e5cd91461afc65b09ebae1bb2e8731
Arnaud MOREL <a.morel@hotmail.com>
2014-04-02 09:33:15 +0200

    Config Less

--- HomeRental/App_Start/BundleConfig.cs
+++ HomeRental/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace HomeRental
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new LessBundle("~/Content/less").Include("~/Content/*.less"));
         }
     }
 }
