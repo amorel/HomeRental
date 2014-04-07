cff7c05914ab2102b1e9cf70ff08489f0bd1c05f
Arnaud MOREL <a.morel@hotmail.com>
2014-04-07 15:13:09 +0200

    Create the Databse Context.

---
+++ HomeRental/DAL/RentalContext.cs
@@ -0,0 +1,16 @@
+﻿using HomeRental.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HomeRental.DAL
+{
+    public class RentalContext : DbContext
+    {
+        public RentalContext() : base("RentalContext")
+        {
+        }
+        
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
