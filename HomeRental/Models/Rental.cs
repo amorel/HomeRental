3267f0eb66205e37f61ac8c3a21b36b4d3111492
Arnaud MOREL <a.morel@hotmail.com>
2014-04-07 11:21:07 +0200

    Add class Reservation and Rental

---
+++ HomeRental/Models/Rental.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HomeRental.Models
+{
+    public class Rental
+    {
+        public int ID { get; set; }
+        public int Capacity { get; set; }
+        public int PricePerNight { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+
+    public class RentalDBContext : DbContext
+    {
+        public DbSet<Rental> Rentals { get; set; }
+    }
+}
---
+++ HomeRental/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HomeRental.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+        public int idUser { get; set; }
+        public int idRental { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class ReservationDBContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
