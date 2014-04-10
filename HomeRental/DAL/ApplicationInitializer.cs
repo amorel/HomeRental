341b7720367f6a925f7fcb9427a0adbfc3b903c2
Arnaud MOREL <a.morel@hotmail.com>
2014-04-10 20:50:03 +0200

    Seed Data Reservation.

--- HomeRental/DAL/ApplicationInitializer.cs
+++ HomeRental/DAL/ApplicationInitializer.cs
@@ -2,6 +2,7 @@ using HomeRental.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Validation;
@@ -44,27 +45,34 @@ namespace HomeRental.DAL
             }
             base.Seed(context);
 
-            //***************************
-            //Seed Data in Rentals Context
-            //***************************
+            //************************************
+            //Seed Data in Rentals Context (Table)
+            //************************************
             var Rentals = new List<Rental>
             {
                 new Rental{ID=1,Capacity=2,PricePerNight=80,GroupPhotoId=null,PropertyType=PropertyType.House,Description="Desc1",Address="chemin de putdael",number=12,PostalCode=1160,City="Auderghem",Country="Belgium",Longitude=null, Latitude=null},
                 new Rental{ID=2,Capacity=1,PricePerNight=60,GroupPhotoId=null,PropertyType=PropertyType.Apartment,Description="Desc2",Address="rue Auguste Orts",number=25,PostalCode=1000,City="Bruxelles",Country="Belgium",Longitude=null, Latitude=null},
                 new Rental{ID=3,Capacity=2,PricePerNight=120,GroupPhotoId=null,PropertyType=PropertyType.House,Description="Desc3",Address="Corniche Verte",number=9,PostalCode=1150,City="Woluwe-Saint-Pierre",Country="Belgium",Longitude=null, Latitude=null},
             };
-
             Rentals.ForEach(s => context.Rentals.Add(s));
             context.SaveChanges();
 
-            //var Reservations = new List<Reservation>
-            //{
-            //    new Reservation{ID=1,ApplicationUserId=null,RentalId=1,StartingDate=DateTime.Parse("2014-08-01"),EndDate=DateTime.Parse("2014-08-05")},
-            //    new Reservation{ID=2,ApplicationUserId=null,RentalId=2,StartingDate=DateTime.Parse("2014-08-12"),EndDate=DateTime.Parse("2014-20-05")},
-            //    new Reservation{ID=3,ApplicationUserId=null,RentalId=3,StartingDate=DateTime.Parse("2014-07-06"),EndDate=DateTime.Parse("2014-07-05")},
-            //};
-            //Reservations.ForEach(s => context.Reservations.Add(s));
-            //context.SaveChanges();
+            //***************************************
+            //Seed Data in Reservation Context (Table)
+            //****************************************
+
+            string user1Id = UserManager.FindByName("user1").Id;
+            string user2Id = UserManager.FindByName("user2").Id;
+            string user3Id = UserManager.FindByName("user3").Id;
+
+            var Reservations = new List<Reservation>
+            {
+                new Reservation{ID=1,ApplicationUserId=user1Id,RentalId=1,StartingDate=DateTime.Parse("2014-01-08"),EndDate=DateTime.Parse("2014-05-08")},
+                new Reservation{ID=2,ApplicationUserId=user2Id,RentalId=2,StartingDate=DateTime.Parse("2014-12-08"),EndDate=DateTime.Parse("2014-05-20")},
+                new Reservation{ID=3,ApplicationUserId=user3Id,RentalId=3,StartingDate=DateTime.Parse("2014-06-07"),EndDate=DateTime.Parse("2014-05-07")},
+            };
+            Reservations.ForEach(s => context.Reservations.Add(s));
+            context.SaveChanges();
 
         }
     }
