28fbc7a21bd71b25b1c4b3e6632af522519e18dd
Arnaud MOREL <a.morel@hotmail.com>
2014-04-10 15:09:38 +0200

    SetInitializer of Database from Global.asax.cs

--- HomeRental/Global.asax.cs
+++ HomeRental/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HomeRental.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,7 @@ namespace HomeRental
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<ApplicationDbContext>(new System.Data.Entity.CreateDatabaseIfNotExists<ApplicationDbContext>());
         }
     }
 }
