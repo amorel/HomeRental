29561988f4afb634441b77cd2cd9eb786338cde7
Arnaud MOREL <a.morel@hotmail.com>
2014-04-16 18:22:48 +0200

    Change DatePicker. Disable dates in the past and the dependent disabling for the next Datepicker.

--- HomeRental/App_Start/BundleConfig.cs
+++ HomeRental/App_Start/BundleConfig.cs
@@ -21,10 +21,12 @@ namespace HomeRental
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/datepicker.css",
                       "~/Content/site.css"));
 
             bundles.Add(new LessBundle("~/Content/less").Include("~/Content/*.less"));
