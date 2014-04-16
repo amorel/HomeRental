f77675c82de4da439f16b0a366867d3c43b88706
Arnaud MOREL <a.morel@hotmail.com>
2014-04-16 22:23:31 +0200

    Adding autocomplete feature  through Google Places API.

---
+++ HomeRental/Scripts/pagehome.js
@@ -0,0 +1,66 @@
+﻿jQuery(document).ready(function () {
+    initselpick();
+    initdatepick();
+    initautocomplete();
+});
+
+
+/*
+*  Select picker to choose the number of guest
+*/
+function initselpick() {
+    $('.selectpicker').selectpicker();
+}
+
+
+/*
+* Datepicker to select 2 dates for interval
+*/
+function initdatepick() {
+    var nowTemp = new Date();
+    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+    var checkin = $('#checkin').datepicker({
+        onRender: function (date) {
+            return date.valueOf() < now.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function (ev) {
+        if (ev.date.valueOf() > checkout.date.valueOf()) {
+            var newDate = new Date(ev.date)
+            newDate.setDate(newDate.getDate() + 1);
+            checkout.setValue(newDate);
+        }
+        checkin.hide();
+        $('#checkout')[0].focus();
+    }).data('datepicker');
+    var checkout = $('#checkout').datepicker({
+        onRender: function (date) {
+            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function (ev) {
+        checkout.hide();
+    }).data('datepicker');
+}
+
+/*
+* using the autocomplete feature of the Google Places API to help users fill in the information.
+*/
+function initautocomplete() {
+    var placeSearch, autocomplete;
+    var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+    };
+    initialize();
+}
+
+function initialize() {
+    // Create the autocomplete object
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
+        { types: ['geocode'] });
+}
