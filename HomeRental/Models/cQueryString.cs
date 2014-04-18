04f4cb7374fa901cd011d1f2d2ce97bdb64b64f3
Arnaud MOREL <a.morel@hotmail.com>
2014-04-19 00:56:03 +0200

    Adding method in cQueryString class to get model from view.

--- HomeRental/Models/cQueryString.cs
+++ HomeRental/Models/cQueryString.cs
@@ -8,5 +8,16 @@ namespace HomeRental.Models
         public DateTime? checkin { get; set; }
         public DateTime? checkout { get; set; }
         public int? guests { get; set; } 
+
+        public String getShortDateCheckin()
+        {
+            String date = checkin.HasValue ? checkin.Value.Date.ToShortDateString():"";
+            return date;
+        }
+        public String getShortDateCheckout()
+        {
+            String date = checkout.HasValue ? checkout.Value.Date.ToShortDateString() : "";
+            return date;
+        }
     }
 }
