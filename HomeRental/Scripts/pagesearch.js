0f56b1fc2d1f952e5694fb4ab945d2b926d88c30
Arnaud MOREL <a.morel@hotmail.com>
2014-05-15 13:57:39 +0200

    Correct Date.

--- HomeRental/Scripts/pagesearch.js
+++ HomeRental/Scripts/pagesearch.js
@@ -75,7 +75,7 @@ function getAddress() {
 }
 
 /*
-*  Selection of the correct parameter in selectpicker (nb Guests)
+*  Selection of the correct parameter Datepicker & selectpicker (nb Guests)
 */
 function filterpara(checkin, checkout, guests) {
     //init datepicker
@@ -87,12 +87,10 @@ function filterpara(checkin, checkout, guests) {
 
     //Select of the correct date parameter on localhost
     if (checkin != "" && document.location.hostname == "localhost") {
-        var date = new Date(checkin.substring(6, 10), checkin.substring(3, 5), checkin.substring(0, 2));
-        $('#checkin').val(date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear());
+        $('#checkin').val(checkin.substring(3, 5) + "/" + checkin.substring(0, 2) + "/" + checkin.substring(6, 10));
 
         if (checkout != "") {
-            var date = new Date(checkout.substring(6, 10), checkout.substring(3, 5), checkout.substring(0, 2));
-            $('#checkout').val(date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear());
+            $('#checkout').val(checkin.substring(3, 5) + "/" + checkin.substring(0, 2) + "/" + checkin.substring(6, 10));
         }
 
     }
@@ -162,7 +160,6 @@ function initializeGoogleMaps() {
 
                 map.fitBounds(bounds);
                 var latlng = results[0].geometry.location;
-                console.log(latlng.toString());
             }
         });
     });
@@ -174,14 +171,6 @@ function initializeGoogleMaps() {
 
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
-
-
-    //var marker = new google.maps.Marker({
-    //    position: new google.maps.LatLng(50.8193813, 4.43914150000001),
-    //    map: map,
-    //    title: 'Hello World!'
-    //});
-
 
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
         var place = autocomplete.getPlace();
@@ -196,9 +185,7 @@ function initializeGoogleMaps() {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
         }
-
         
-
         var address = '';
         if (place.address_components) {
             address = [
