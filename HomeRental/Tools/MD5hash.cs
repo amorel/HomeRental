4757631131c220c768c3eedf1c87b96ac5d12ca2
Arnaud MOREL <a.morel@hotmail.com>
2014-05-17 15:09:40 +0200

    Adding MD5hash class to convert email address to MD5 hash.

---
+++ HomeRental/Tools/MD5hash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRental.Tools
+{
+    /// <summary>
+    /// calculate a MD5 hash from a string
+    /// </summary>
+    public class MD5hash
+    {
+        public string CalculateMD5Hash(string input)
+        {
+            input = input.Replace(" ", string.Empty).ToLower();
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
